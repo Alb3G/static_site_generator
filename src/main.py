18bb0935dfd267d4f45302c99d7d691996646223
Alb3G <guzmanmorenoalberto1998@gmail.com>
2025-07-16 22:44:01 +0200

    feat: :sparkles: Added function to generate and deploy an entire html page dinamically
    Added function to generate and deploy an entire html page dinamically

--- src/main.py
+++ src/main.py
@@ -4,23 +4,26 @@ from utils.markdown_to_htmlnode.markdown_to_html_node import markdown_to_html_no
 from utils.extract_markdown_title.extract_markdown_title import extract_title
 
 def gen_page(from_path: str, template_path: str, dest_path: str):
-	print(f"Generating page from {from_path} to {dest_path}, using {template_path}")
+	from_path_file_name = os.path.dirname(from_path)
+	dest_path_file_name = os.path.dirname(dest_path)
+	template_path_file_name = os.path.dirname(template_path)
+	print(f"Generating page from {from_path_file_name} to {dest_path_file_name}, using {template_path_file_name}")
 	# read md and template files
-	md_file_content = ""
-	template = ""
 	with open(from_path, "r", encoding="utf-8") as file:
 		md_file_content = file.read()
 	with open(template_path, "r", encoding="utf-8") as file:
 		template = file.read()
 	# transform text to html content
 	html_from_from_md = markdown_to_html_node(md_file_content).to_html()
-	md_title = extract_title(md_file_content)
+	md_title = extract_title(md_file_content).replace("#", "").strip()
 	# replace template tags for new generated content
-	template.replace("{{ Title }}", md_title)
-	template.replace("{{ Content }}", html_from_from_md)
+	template = template.replace("{{ Title }}", md_title).replace("{{ Content }}", html_from_from_md)
 	# write the new template
+	dest_path_dirs = os.path.dirname(dest_path)
+	os.makedirs(dest_path_dirs, exist_ok=True)
 	with open(dest_path, "w", encoding="utf-8") as file:
 		file.write(template)
+
 	
 
 def gen_build(src: str, dest: str):
@@ -41,7 +44,14 @@ def gen_build(src: str, dest: str):
 def main():
 	src = "/Users/albertoguzman/Code/static_site_generator/static"
 	dest = "/Users/albertoguzman/Code/static_site_generator/public"
+	template_path = "/Users/albertoguzman/Code/static_site_generator/template.html"
+	main_content_path = "/Users/albertoguzman/Code/static_site_generator/content/content.md"
+	glorfindel_content_path = "/Users/albertoguzman/Code/static_site_generator/content/blog/glorfindel/index.md"
+	template_dest_path = f"{dest}/index.html"
+	glorfindel_template_dest_path = f"{dest}/blog/glorfindel.html"
 	gen_build(src, dest)
+	gen_page(main_content_path, template_path, template_dest_path)
+	gen_page(glorfindel_content_path, template_path, glorfindel_template_dest_path)
 
 if __name__ == "__main__":
 	main()
--- src/utils/extract_markdown_title/extract_markdown_title.py
+++ src/utils/extract_markdown_title/extract_markdown_title.py
@@ -1,11 +1,11 @@
 import re
 
 def extract_title(markdown: str) -> str:
-	h1: list[str] = re.findall(r"^\s*(#\s+.*)", markdown, re.MULTILINE);
+	h1: list[str] = re.findall(r"^\s*(#\s+.*)", markdown, re.MULTILINE)
 
 	if len(h1) == 0:
-		raise Exception("No h1 found in the markdown!");
+		raise Exception("No h1 found in the markdown!")
 	
 	result = h1[0].strip()
 
-	return result;
+	return result
